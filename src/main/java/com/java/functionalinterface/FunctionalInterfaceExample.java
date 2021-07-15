7f9b3d46e6c013e825bb9b6ded9882df5c7ad481
premnathpedduri <premnath.pedduri@gmail.com>
2021-07-15 18:53:16 +0530

    testing some sceariios

--- src/main/java/com/java/functionalinterface/FunctionalInterfaceExample.java
+++ src/main/java/com/java/functionalinterface/FunctionalInterfaceExample.java
@@ -3,7 +3,7 @@ package com.java.functionalinterface;
 public class FunctionalInterfaceExample {
 	public static void main(String[] args) {
 		Square s = new Square();
-		int i = 4;
+		int i = 5;
 		System.out.println("Square of :" + i + " is : " + s.calculate(4));
 	}
 }
